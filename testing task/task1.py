b909214fc787d2bfcc69b1a4d788f5e9ca979511
SelesPurple <kazanovanass@mail.ru>
2023-09-17 01:09:04 +0300

    ex

--- testing task/task1.py
+++ testing task/task1.py
@@ -2,6 +2,19 @@ N, M = input('Введите через пробел количество мес
 N = int(N)
 M = int(M)
 Cn = [0]*N
+
 print('Введите через Enter количество печенек в каждом месте:')
 for i in range(len(Cn)):
     Cn[i] = int(input())
+
+def search(n, m, cn):
+    if m < n:
+        return 0
+    if n == n:
+        return max(cn)
+
+print(search(N, M, Cn))
+
+
+
+
